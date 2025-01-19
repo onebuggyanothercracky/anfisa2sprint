9dfb4679d31e4965163c9df8e9eaccd24edf57c1
Buggu <last.coin@yandex.ru>
2025-01-20 00:50:30 +0300

    global update

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -60,10 +60,16 @@ class IceCream(PublishedModel):
     )
     toppings = models.ManyToManyField(Topping, verbose_name="Топпинг")
     is_on_main = models.BooleanField(default=False, verbose_name="На главную")
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         verbose_name = "Мороженое"
         verbose_name_plural = "Мороженое"
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
